24a3aae7e8bef95613a9ee1c78215464de7b9808
fliptheweb <itisshortemailaddress@gmail.com>
2016-05-27 22:34:32 +0700

    first test

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const Z_MAX = 6       // Maximum z value
+const Z_MAX = 6; // Maximum z value
 
 let abCalculator = {
   /*
@@ -15,7 +15,7 @@ let abCalculator = {
     n2 = sample size for group #2
   */
   getSampleSize: (data) => {
-    let {alpha, beta, convertion1, convertion2, ratio, trafficMax} = abCalculator._normalizeData(data);
+    let {alpha, beta, convertion1, convertion2, ratio} = abCalculator._normalizeData(data);
 
     let delta = Math.abs(convertion1 - convertion2);
     let q1 = 1 - convertion1;
@@ -26,8 +26,16 @@ let abCalculator = {
     let zValue2 = abCalculator._computeCriticalNormalZValue(1 - beta);
 
     console.log(delta, q1, q2, pResult, qResult, zValue1, zValue2);
-    let n1 = Math.pow((zValue1 * Math.sqrt(pResult * qResult * (1 + 1 / ratio)) + zValue2 * Math.sqrt(convertion1 * q1 + ((convertion2 * q2) / ratio))), 2) / Math.pow(delta, 2);
+    let n1 = Math.pow(
+      (zValue1 * Math.sqrt(
+        pResult * qResult * (1 + 1 / ratio)
+      ) + zValue2 * Math.sqrt(
+        convertion1 * q1 + ((convertion2 * q2) / ratio))
+      ),
+      2
+    ) / Math.pow(delta, 2);
 
+    // winner?
     return {
       group1: parseInt(n1, 10),
       group2: parseInt(ratio * n1, 10)
@@ -98,20 +106,20 @@ let abCalculator = {
       } else if (y < 1.0) {
         w = y * y;
         x = ((((((((0.000124818987 * w -
-                  0.001075204047) * w + 0.005198775019) * w -
-                  0.019198292004) * w + 0.059054035642) * w -
-                  0.151968751364) * w + 0.319152932694) * w -
-                  0.531923007300) * w + 0.797884560593) * y * 2.0;
+                   0.001075204047) * w + 0.005198775019) * w -
+                   0.019198292004) * w + 0.059054035642) * w -
+                   0.151968751364) * w + 0.319152932694) * w -
+                   0.531923007300) * w + 0.797884560593) * y * 2.0;
       } else {
         y -= 2.0;
         x = (((((((((((((-0.000045255659 * y +
-                        0.000152529290) * y - 0.000019538132) * y -
-                        0.000676904986) * y + 0.001390604284) * y -
-                        0.000794620820) * y - 0.002034254874) * y +
-                        0.006549791214) * y - 0.010557625006) * y +
-                        0.011630447319) * y - 0.009279453341) * y +
-                        0.005353579108) * y - 0.002141268741) * y +
-                        0.000535310849) * y + 0.999936657524;
+                         0.000152529290) * y - 0.000019538132) * y -
+                         0.000676904986) * y + 0.001390604284) * y -
+                         0.000794620820) * y - 0.002034254874) * y +
+                         0.006549791214) * y - 0.010557625006) * y +
+                         0.011630447319) * y - 0.009279453341) * y +
+                         0.005353579108) * y - 0.002141268741) * y +
+                         0.000535310849) * y + 0.999936657524;
       }
     }
 
---
+++ test/test.js
@@ -0,0 +1,6 @@
+import test from 'ava';
+import abCalculator from '../src/main';
+
+test('try tests', t => {
+  t.pass();
+});
