6d1fff050c85d7de9025bb2e68a8a6138ebf3c0c
fliptheweb <itisshortemailaddress@gmail.com>
2016-08-27 16:47:42 +0300

    feat(ui): add simple ui

--- src/browser.js
+++ src/browser.js
@@ -7,7 +7,7 @@ let initData = {
   beta: 20,  // optional
   convertions: [1.2, 1.4],
   currentGroupSizes: [], // optional
-  deltaConvertion: ''   // optional
+  deltaConvertion: 5   // optional
   // maxTrafficPerGroup: 10000
 }
 
@@ -15,8 +15,11 @@ const RESULTS = {
   WINNER_FIRST: 'First variant are winner.',
   WINNER_SECOND: 'Second variant are winner.',
   WINNER_EQUAL: 'Both variants are equal.',
-  NOT_ENOUGH: (strings) => `It\`s not enough traffic in groups, need ${strings[0]}.`,
-  NO_CURRENT_GROUP: 'We can`t say winner, because it`s relative to the group size.'
+  NOT_ENOUGH: (strings) =>
+    `It\`s not enough traffic in groups, need ${strings[0]}.`,
+  NO_CURRENT_GROUP: 'We can`t detect winner, because it`s relative to the group size.',
+  NOT_MINIMUM_DELTA_CONVERTION: ([deltaConvertion, currentDeltaConversion]) =>
+    `Waiting for minimum delta between conversion (${deltaConvertion}). Current – ${currentDeltaConversion}`
 }
 
 let getTextOfResult = (resultCode, ...params) => {
@@ -38,28 +41,32 @@ let validateData = ({alpha, beta, convertions, currentGroupSizes, deltaConvertio
     }
   }
 
-  if (currentGroupSizes && currentGroupSizes.length === 2) {
-    currentGroupSizes = [parseFloat(currentGroupSizes[0]), parseFloat(currentGroupSizes[1])];
-  } else {
-    currentGroupSizes = undefined;
-  }
-
-  return {
+  let validatedData = {
     alpha: parseFloat(alpha),
     beta: parseFloat(beta),
-    convertions: [parseFloat(convertions[0]), parseFloat(convertions[1])],
-    currentGroupSizes,
-    deltaConvertion: parseFloat(deltaConvertion)
+    convertions: [parseFloat(convertions[0]), parseFloat(convertions[1])]
   }
+
+  if (currentGroupSizes && currentGroupSizes.length === 2) {
+    validatedData.currentGroupSizes = [parseFloat(currentGroupSizes[0]), parseFloat(currentGroupSizes[1])];
+  }
+
+  if (deltaConvertion) {
+    validatedData.deltaConvertion = parseFloat(deltaConvertion);
+  }
+
+  return validatedData;
 }
 
 let ABGroupSizeGui = (params) => {
   let data = validateData(params);
-  // let isNeedMoreTraffic = false;
   let result = {};
+  // определиться как мерить delta, в процентах между конверсиями?
+  let currentDeltaConversion = Math.abs(data.convertions[0] - data.convertions[1]);
 
   // validate data
   result.groupSizes = ABGroupSize(data);
+  result.winner = false;
 
   if (data.deltaConvertion) {
     result.deltaGroupSizes = ABGroupSize(Object.assign(
@@ -87,18 +94,16 @@ let ABGroupSizeGui = (params) => {
       isEnoughData = true
     }
 
-    // if (data.currentGroupSizes[0] >= result.deltaGroupSizes[0] && data.currentGroupSizes[1] >= result.deltaGroupSizes[1]) {
-    //   result.text = 'It`s enough traffic for your delta convertions';
-    //   result.type = 'ENOUGH_TRAFFIC_DELTA';
-    // }
-
     // Определяем победителя
     if (isEnoughData) {
       if (data.convertions[0] > data.conversions[1]) {
+        result.winner = 1;
         result.text = getTextOfResult('WINNER_FIRST');
       } else if (data.convertions[0] < data.conversions[1]) {
+        result.winner = 2;
         result.text = getTextOfResult('WINNER_SECOND');
       } else {
+        result.winner = [1, 2];
         result.text = getTextOfResult('WINNER_EQUAL');
       }
     }
@@ -112,9 +117,65 @@ let ABGroupSizeGui = (params) => {
       }
       result.text = getTextOfResult('NOT_ENOUGH', missingAmount);
     }
+
+    // Если текущая разница конверсий больше, чем нужно, то победитель есть, если нет, то нет
+    if (data.deltaConvertion && data.deltaConvertion > currentDeltaConversion) {
+      result.winner = false;
+      result.text = getTextOfResult('NOT_MINIMUM_DELTA_CONVERTION', data.deltaConvertion, currentDeltaConversion);
+
+      // if (data.currentGroupSizes[0] >= result.deltaGroupSizes[0] && data.currentGroupSizes[1] >= result.deltaGroupSizes[1]) {
+      //   result.text = 'It`s enough traffic for your delta convertions';
+      //   result.type = 'ENOUGH_TRAFFIC_DELTA';
+      // }
+    }
   }
 
-  return result;
+  return Object.assign(
+    {},
+    {initData: data},
+    result
+  )
 }
 
-document.getElementById('result').innerHTML = JSON.stringify(ABGroupSizeGui(initData), null, 2);
+//
+// Format result
+//
+
+let aGroupSize = document.getElementById('a-group-size');
+let bGroupSize = document.getElementById('b-group-size');
+
+// Settings
+let alpha = document.getElementById('alpha');
+let beta = document.getElementById('beta');
+let deltaConvertion = document.getElementById('delta-conversion');
+let maxTraffic = document.getElementById('max-traffic');
+
+let renderCalculator = (event) => {
+  let data = Object.assign(
+    {},
+    initData,
+    {
+      alpha: alpha.value,
+      beta: beta.value,
+      currentGroupSizes: [
+        aGroupSize.value,
+        bGroupSize.value
+      ],
+      convertions: [
+        1.2, 1.4
+      ],
+      maxTrafficPerGroup: maxTraffic.value,
+      deltaConvertion: deltaConvertion.value
+    }
+  )
+  document.getElementById('result').innerHTML = JSON.stringify(ABGroupSizeGui(data), null, 2);
+}
+
+[alpha, beta, deltaConvertion, maxTraffic].forEach((control) => {
+  ['change', 'click', 'keyup'].forEach((eventName) => {
+    control.addEventListener(eventName, renderCalculator);
+  })
+})
+
+// Initial
+renderCalculator()
